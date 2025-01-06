381b3fde26d14707f4d0ea0683916e868fe59964
lDelia43 <ldelia@fi.uba.ar>
2025-01-06 18:03:12 -0300

    feat(apps/native): First update validation page

---
+++ apps/native/app/auth/register/validation/index.tsx
@@ -0,0 +1,55 @@
+import { SizableText, Stack, useTheme, XStack, YStack } from "tamagui"
+import React, { useState } from "react"
+import GlassBottomSheet from "../../../../components/GlassBotomSheet"
+import { Button } from "../../../../components/ui/Button"
+import { Input } from "../../../../components/ui/Input"
+import EyeIcon from "../../../../components/icons/Eye"
+import EyeSlashIcon from "../../../../components/icons/EyeSlash"
+import { BlurView } from "expo-blur"
+
+export default function Validation() {
+  const theme = useTheme()
+  const [showPassword, setShowPassword] = useState(false)
+
+  return (
+    <YStack gap={40} width="100%">
+      <YStack justifyContent="center" alignItems="center" gap={4}>
+        <SizableText textTransform="uppercase" color="$white" size="$headline-large">
+          Cohor
+        </SizableText>
+        <SizableText color="$white" size="$body-medium-w-medium">
+          Explorá nuevas conexiones
+        </SizableText> 
+      </YStack>
+      <GlassBottomSheet>
+        <YStack width="100%" alignItems="center" gap={12}>
+          <SizableText color="$white" size="$headline-small">
+            Verificá tu email
+          </SizableText>
+          <SizableText color="$white-opacity-high" size="$body-small">
+            Te hemos enviado un correo electrónico a{" "}
+            <SizableText color="$white" size="$body-medium-w-medium">
+              santiventura.96@gmail.com
+            </SizableText>{" "}
+            Haz clic en el enlace dentro para comenzar.
+          </SizableText>
+        </YStack>
+        <YStack width="100%" gap={20}>
+          <Button>Abrir email</Button>
+          <BlurView
+            intensity={100}
+            tint="dark"
+            style={{
+              borderRadius: 100,
+              overflow: "hidden"
+            }}
+          >
+            <Button type="subtle" backgroundColor="transparent" borderColor="rgba(255, 255, 255, 0.2)">
+              Reenviar email
+            </Button>
+          </BlurView>
+        </YStack>
+      </GlassBottomSheet>
+    </YStack>
+  )
+}
