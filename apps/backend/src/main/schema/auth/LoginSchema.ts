88f32b7d88e328e1eca089290e0e25d18a579905
santisemhan <santisemhan2@gmail.com>
2025-01-07 21:31:24 -0300

    chore(apps/backend): Stuff

--- apps/backend/src/main/schema/auth/LoginSchema.ts
+++ apps/backend/src/main/schema/auth/LoginSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 
 export const LoginSchema = z.object({
   email: z.string().email(),
-  password: z.string().min(8)
+  password: z.string()
 })
 
 export type RegisterRequest = z.infer<typeof LoginSchema>
