6813d2637f896ea2f888d780ca50677e9b32181c
lDelia43 <ldelia@fi.uba.ar>
2025-01-10 17:15:18 -0300

    feat(apps/native): google auth

--- apps/native/components/templates/auth/login/LoginOptions.tsx
+++ apps/native/components/templates/auth/login/LoginOptions.tsx
@@ -1,10 +1,15 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Separator, SizableText, useTheme, XStack, YStack } from "tamagui"
 import { Button } from "../../../ui/Button"
 import { BlurView } from "expo-blur"
 import MailboxIcon from "../../../icons/Mailbox"
 import GoogleIcon from "../../../icons/GoogleIcon"
 import { Link } from "expo-router"
+import * as WebBrowser from "expo-web-browser"
+import * as Google from "expo-auth-session/providers/google"
+import * as AuthSession from "expo-auth-session"
+
+WebBrowser.maybeCompleteAuthSession()
 
 interface LoginOptionsProps {
   // TODO: Add to context
@@ -12,7 +17,36 @@ interface LoginOptionsProps {
 }
 
 export default function LoginOptions({ setAuthFlow }: LoginOptionsProps) {
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    webClientId: "300195889471-o655ub38lc5e0obfh9hicqpecundh0hg.apps.googleusercontent.com",
+    iosClientId: "300195889471-lka9cqqind6u4hloqdcsl4vne93nt5gg.apps.googleusercontent.com",
+    androidClientId: "300195889471-gebukno7f83ouj36me59bghuuquf4onj.apps.googleusercontent.com",
+    redirectUri: AuthSession.makeRedirectUri({ native: "https://auth.expo.io/cohor/mobile" })
+  })
+  const [accessToken, setAccessToken] = useState<string | null>(null)
+  const [user, setUser] = useState()
   const theme = useTheme()
+
+  useEffect(() => {
+    if (response && response?.type === "success") {
+      const { authentication } = response
+      if (authentication) {
+        setAccessToken(authentication.accessToken)
+        accessToken && fetchUserInfo()
+      }
+      console.log("Token de Google:", authentication?.accessToken)
+    }
+  }, [response, accessToken])
+
+  const fetchUserInfo = async () => {
+    let response = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
+    const userInfo = await response.json()
+    setUser(userInfo)
+  }
 
   return (
     <>
@@ -20,7 +54,11 @@ export default function LoginOptions({ setAuthFlow }: LoginOptionsProps) {
         Iniciar Sesión
       </SizableText>
       <YStack gap={24} width="100%">
-        <Button backgroundColor="$element-high" icon={<GoogleIcon width={21} height={21} />}>
+        <Button
+          onPress={() => promptAsync()}
+          backgroundColor="$element-high"
+          icon={<GoogleIcon width={21} height={21} />}
+        >
           Continuar con Google
         </Button>
         <XStack justifyContent="center" alignItems="center" gap={8}>
