937ddb3e6158e387fe09b7a2365c7d46581d808f
santisemhan <santisemhan2@gmail.com>
2025-02-25 17:48:12 -0300

    feat: Empty Chats Illustration

--- apps/native/app/app/(tabs)/chat.tsx
+++ apps/native/app/app/(tabs)/chat.tsx
@@ -2,7 +2,7 @@ import { SafeAreaView } from "react-native-safe-area-context"
 import { useTheme } from "tamagui"
 import EmptyState from "../../../components/app/common/EmptyState"
 import MainHeader from "../../../components/app/common/MainHeader"
-import UserIcon from "../../../components/icons/User"
+import EmptyEvents from "../../../components/illustations/EmptyEvents"
 
 export default function Chat() {
   const theme = useTheme()
@@ -12,7 +12,7 @@ export default function Chat() {
       <EmptyState
         title="Todavía no hay chats grupales"
         subtitle="Cuando hagas match con otro grupo, se creará automáticamente un chat para que puedan conocerse mejor."
-        ilustration={<UserIcon />}
+        ilustration={<EmptyEvents />}
       />
     </SafeAreaView>
   )
