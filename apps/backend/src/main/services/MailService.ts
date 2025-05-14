05fa84f60ebaa7c50ab9ecf6808aabfd3c257728
santisemhan <santisemhan2@gmail.com>
2025-05-13 22:35:00 -0300

    feat: New subdomain for transactional email

--- apps/backend/src/main/services/MailService.ts
+++ apps/backend/src/main/services/MailService.ts
@@ -23,7 +23,7 @@ export class MailService {
     const { to, subject, body, from } = data
     try {
       const client = await this.getClientAsync()
-      const emailFrom = from || "Cohor <no-reply@cohor.app>"
+      const emailFrom = from || "Cohor <no-reply@notify.cohor.app>"
       await client.emails.send({
         from: emailFrom,
         to,
